5b6c731abc3bb5e1faaeb025dcd0cfcf15bf8a14
yandeu <20306025+yandeu@users.noreply.github.com>
2022-05-24 00:11:44 +0200

    dev

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,11 +5,30 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
+const pause = (ms: number): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), ms);
+  });
+};
+
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
 
   test("Sample test", () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+
+  test("open five-server", async () => {
+    const res1 = await vscode.commands.executeCommand("fiveServer.open");
+    assert.equal(res1, "done");
+
+    await pause(5000);
+
+    const res2 = await vscode.commands.executeCommand("fiveServer.close");
+    assert.equal(res2, "done");
+
+    await pause(5000);
+    return;
   });
 });
